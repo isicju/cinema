b23154d75c942469fce0c62c0682531ada87fd7c
Anatoly Malozemov <glenglen74@gmail.com>
2023-11-04 12:17:46 +0500

    add cinema-8 task

---
+++ src/main/java/com/vkatit/cinema/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.vkatit.cinema.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return String.format("Hello World!");
+    }
+
+}
