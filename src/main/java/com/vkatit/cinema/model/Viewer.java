b5a81cc5cbda7c497fd29fdf70931c7ae43d6500
David <david.rtgr@gmail.com>
2023-11-19 18:08:31 +0300

    Added Ticket PDF Generator service with basic functions and field filling for date, seat number, user name, and movie name. defaultTicket file added, ticket files with seats had been removed

---
+++ src/main/java/com/vkatit/cinema/model/Viewer.java
@@ -0,0 +1,11 @@
+package com.vkatit.cinema.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Viewer {
+    private String firstName;
+    private String lastName;
+}
